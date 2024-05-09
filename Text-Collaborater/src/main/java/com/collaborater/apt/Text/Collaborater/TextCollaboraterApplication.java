c25fcefe668aa0d6b1d413ca0673ba1ea8fe6a3c
OmarEssam98 <76596326+OmarEssam98@users.noreply.github.com>
2024-05-09 18:04:06 +0300

    Add files via upload

---
+++ Text-Collaborater/src/main/java/com/collaborater/apt/Text/Collaborater/TextCollaboraterApplication.java
@@ -0,0 +1,13 @@
+package com.collaborater.apt.Text.Collaborater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextCollaboraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextCollaboraterApplication.class, args);
+	}
+
+}
